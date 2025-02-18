1c8cd61b56d5dec94e2fe0b4d4a670e49aceaef4
Ahmed8881 <buttahmed2004@gmail.com>
2025-02-18 22:35:35 +0500

    Add vertical stacking and horizontal splitting examples to reshaping.py; create bool_indexing.py for boolean indexing operations

--- 6-Reshaping/reshaping.py
+++ 6-Reshaping/reshaping.py
@@ -14,3 +14,7 @@ print("Original arr2:\n", arr2)
 print("Horizontal stacking:\n", np.hstack((arr1, arr2)))
 #vstack
 print("Vertical stacking:\n", np.vstack((arr1, arr2)))
+#hsplit
+arr3 = np.arange(24).reshape(4, 6)
+print("Original array:\n", arr3)
+print("Horizontal splitting:\n", np.hsplit(arr3, 2))
