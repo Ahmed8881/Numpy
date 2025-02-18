606137115e9d90460ab5885954cec63dabc45117
Ahmed8881 <buttahmed2004@gmail.com>
2025-02-18 22:07:40 +0500

    Add reshaping.py with examples of NumPy array reshaping operations

---
+++ 6-Reshaping/reshaping.py
@@ -0,0 +1,16 @@
+#Reshaping arrrays
+import numpy as np
+#ravel fun converts higher dim array in one dim array
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+print("Original array:\n", arr)
+print("Flattened array:", arr.ravel())
+#transpose fun converts rows into columns and columns into rows
+print("Transposed array:\n", arr.transpose())
+#hstack
+arr1=np.arange(6).reshape(2,3)
+arr2=np.arange(6,12).reshape(2,3)
+print("Original arr1:\n", arr1)
+print("Original arr2:\n", arr2)
+print("Horizontal stacking:\n", np.hstack((arr1, arr2)))
+#vstack
+print("Vertical stacking:\n", np.vstack((arr1, arr2)))
