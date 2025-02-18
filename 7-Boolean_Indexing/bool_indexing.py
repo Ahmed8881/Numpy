7a75b07b113e286196d6b664d44f80ae8a1f2ee0
Ahmed8881 <buttahmed2004@gmail.com>
2025-02-18 22:42:35 +0500

    Add examples of boolean indexing operations in bool_indexing.py

--- 7-Boolean_Indexing/bool_indexing.py
+++ 7-Boolean_Indexing/bool_indexing.py
@@ -1,1 +1,6 @@
 import numpy as np
+arr=np.random.randint(1, 100, 20).reshape(4, 5)
+print("Original array:\n", arr)
+print("Boolean mask where elements > 50:\n", arr > 50)
+print("Elements greater than 50:", arr[arr > 50])
+print(arr[arr > 50 & (arr%2==0)])
