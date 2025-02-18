8018974c771f08ebec7dcba886a2cc1a393f3d3e
Ahmed8881 <buttahmed2004@gmail.com>
2025-02-18 21:36:57 +0500

    Add examples for indexing and iteration with NumPy arrays

---
+++ 4-Indexing/indexing.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8,9,10])
+arr1=np.arange(24).reshape(6,4)
+print(arr1)
+print(arr[3])
+print(arr[2:3])
+print(arr1[1:3,1:3])
+print(arr1[:,2])
---
+++ 4-Indexing/iteration.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr12=np.arange(24).reshape(6,4)
+for i in arr12:
+    print(i)
+# to print numbers seperately
+    print(i)
