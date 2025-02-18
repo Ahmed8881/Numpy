54b76786ca008e59909cae7ce9ea461bbb4f48a4
Ahmed8881 <buttahmed2004@gmail.com>
2025-02-18 22:47:48 +0500

    Add plot_graphs.py for visualizing sine function using NumPy and Matplotlib

---
+++ 8-Plotting_Graphs/plot_graphs.py
@@ -0,0 +1,15 @@
+import numpy as np  # Importing the numpy library for numerical operations
+import matplotlib.pyplot as plt  # Importing the matplotlib library for plotting graphs
+
+
+# Creating an array of 100 linearly spaced values between -40 and 40
+x = np.linspace(-40, 40, 100)
+
+# Calculating the sine of each value in the array x
+y = np.sin(x)
+
+# Plotting the values of x and y
+plt.plot(x, y)
+
+# Displaying the plot
+plt.show()
